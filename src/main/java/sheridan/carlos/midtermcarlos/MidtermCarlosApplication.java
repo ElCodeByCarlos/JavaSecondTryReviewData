ec0efd6bd5edb01b288ea0bbbbae789e64e724dd
Carlos Tello Dominguez <tellodom@sheridancollege.ca>
2026-02-17 15:22:42 -0500

    First Commit

---
+++ src/main/java/sheridan/carlos/midtermcarlos/MidtermCarlosApplication.java
@@ -0,0 +1,13 @@
+package sheridan.carlos.midtermcarlos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermCarlosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MidtermCarlosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/carlos/midtermcarlos/MidtermCarlosApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.carlos.midtermcarlos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermCarlosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
